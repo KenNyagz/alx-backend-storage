162ab3a84a230698670f1dae632462d16cdad690
kennyagz <kennyagz681@gmail.com>
2024-06-14 20:42:36 +0000

    LOOP iteration SQL

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,8 +1,29 @@
 -- creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
 
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 
-DELIMITER //
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+        DECLARE tot_weighted_score FLOAT;
+        DECLARE tot_weight FLOAT;
+
+        SELECT SUM(corrections.score * projects.weight), SUM(projects.weight)
+        INTO tot_weighted_score, tot_weight
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+        IF tot_weight > 0 THEN
+                UPDATE users
+                SET average_score = tot_weighted_score / tot_weight
+                WHERE users.id = user_id;
+        END IF;
+
+END$$
+
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
@@ -20,6 +41,6 @@ BEGIN
 		CALL ComputeAverageWeightedScoreForUser(user_id);
 	END LOOP;
 	CLOSE cur;
-END//
+END$$
 
 DELIMITER ;
