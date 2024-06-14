3a8444a5b4c32a75ebd3b96a04a1745a4a821d07
kennyagz <kennyagz681@gmail.com>
2024-06-14 06:21:31 +0000

    Sum column

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,4 +4,4 @@ SELECT band_name,
    IFNULL(DATEDIFF(IFNULL(split, 2022), formed), IFNULL(2022 - formed, NULL)) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan;
+ORDER BY lifespan DESC;
--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -6,7 +6,7 @@ AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
+    SET quantity = quantity - NEW.quantity_ordered
     WHERE name = NEW.item_name;
 END //
 
