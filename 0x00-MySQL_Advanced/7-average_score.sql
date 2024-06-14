b9a0f0fccb4856b9b804df4a8e2a42b5033b5b33
kennyagz <kennyagz681@gmail.com>
2024-06-14 12:32:45 +0000

    average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,17 +3,25 @@
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
 DELIMITER //
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE avg_score FLOAT;
+	DECLARE total_score FLOAT;
+	DECLARE num_projects INT;
 
-	SELECT AVG(score) INTO avg_score
+	SELECT SUM(score) AS total_score, COUNT(*) AS num_projects
 	FROM corrections
 	WHERE user_id = user_id;
 
-	UPDATE users
-	SET average_score = avg_score
-	WHERE id = user_id;
+	IF num_projects > 0 THEN
+		UPDATE users
+		SET average_score = total_score / num_projects
+		WHERE id = user_id;
+	ELSE
+		UPDATE users
+		SET average_score = 0
+		WHERE id = user_id;
+	END IF;
 END//
 
 DELIMITER ;
