c1a69743524beb3e1a15c2cd3fe2b40c7e4ef926
kennyagz <kennyagz681@gmail.com>
2024-06-14 06:43:32 +0000

    IFNULL check

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,5 @@
 SELECT band_name,
    IFNULL(DATEDIFF(IFNULL(split, 2022), formed), IFNULL(2022 - formed, NULL)) AS lifespan
 FROM metal_bands
-WHERE style = '%Glam rock%'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
