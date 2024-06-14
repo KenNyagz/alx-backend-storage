7526f17bcd02c54c8f6066d134e9370a871722d1
kennyagz <kennyagz681@gmail.com>
2024-06-14 06:24:10 +0000

    band lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
 
 SELECT band_name,
-   IFNULL(DATEDIFF(IFNULL(split, 2022), formed), IFNULL(2022 - formed, NULL)) AS lifespan
+   --IFNULL(DATEDIFF(IFNULL(split, 2022), formed), IFNULL(2022 - formed, NULL)) AS lifespan
+   (2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
