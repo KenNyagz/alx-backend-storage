13591fae8d17b7ad8e3dbd06fa90ff7bd5afa675
KenNyagz <kennyagz681@gmail.com>
2024-06-19 18:21:03 +0300

    class for initializing redis db objs and storing data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,11 +11,12 @@ class Cache:
     '''Class for defining redis cache objects'''
     def __init__(self):
         '''Initialize Redis client and flush the database'''
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Generate random key and save the data to that key'''
         key = str(uuid.uuid4())
-        self.__redis.set(key, data)
+        self._redis.set(key, data)
+        #self._redis.set(key, str(data).encode())
         return key
