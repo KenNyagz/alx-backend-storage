d072ef76b669b7e27d1ab793e4e2e439c09f0072
KenNyagz <kennyagz681@gmail.com>
2024-06-19 22:40:39 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Class for redis databases and the methods involved in managing the data
 '''
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 import uuid
 
 
@@ -18,5 +18,28 @@ class Cache:
         '''Generate random key and save the data to that key'''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
-        #self._redis.set(key, str(data).encode())
+        # self._redis.set(key, str(data).encode())
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, int, float, bytes]]:
+        '''Reading from Redis and recovering original type'''
+        data = self._redis.get(key)
+        if data is None:
+            return None
+
+        if fn:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        '''Gets string version of data retrieved'''
+        #return self.get(key, lambda x: x.decode('utf-8') if x else None)
+        result = self.get(key, lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
+        return result if isinstance(result, str) else None
+
+    def get_int(self, key: str) -> Optional[int]:
+        '''gets integer version of data'''
+        #return self.get(key, lambda x: int(x) if x else None)
+        result = self.get(key, lambda x: int(x) if isinstance(x, (bytes, str)) else x)
+        return result if isinstance(result, int) else None
