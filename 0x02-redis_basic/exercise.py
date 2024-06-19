431fa69fce4842d8878eaed2e492014f45cd9858
KenNyagz <kennyagz681@gmail.com>
2024-06-19 23:43:57 +0300

    Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,21 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    '''add its input parameters to one list in redis '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''decorator function'''
+        input_key = f'{method.__qualname__}:inputs'
+        output_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(result))
+        return result
+    return wrapper
+
+
 class Cache:
     '''Class for defining redis cache objects'''
     def __init__(self):
@@ -28,6 +43,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Generate random key and save the data to that key'''
         key = str(uuid.uuid4())
