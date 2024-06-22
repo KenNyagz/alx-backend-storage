e12b9f9cb798b369bb46a71d8dbfbf5528bc724d
KenNyagz <kennyagz681@gmail.com>
2024-06-22 13:27:51 +0300

    $aggregate (pipelines)

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''function that returns all students sorted by average score'''
+from pymongo import MongoClient
+
+
+def top_students(mongo_collection):
+    '''returns stidents sorted by avg score'''
+    pipeline = [
+        {
+            '$project': {
+                'name': 1,
+                'averageScore': { '$avg': '$scores.scores' }
+            }
+        },
+        {
+            '$sort': { 'averageScore': -1 }
+        }
+    ]
+    result = mongo_collection.aggregate(pipeline)
+
+    return list(result)
